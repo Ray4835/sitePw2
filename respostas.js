059a23351f58a68a64e46d982c92229173e5f616
Rayssa <115266145+Ray4835@users.noreply.github.com>
2023-04-11 22:32:10 -0300

    Update respostas.js

--- respostas.js
+++ respostas.js
@@ -6,62 +6,62 @@ score= score + 1;
 certo1.style.color="#00ff00"
 }
 if (document.form2.e21[1].checked) {
-erradoll.style.color= "#ff0000"
+errado11.style.color= "#ff0000"
 }
 if (document.form2.e21[2].checked) {
-erradol2.style.color= "#ff0000"
+errado12.style.color= "#ff0000"
 }
 if (document.form2.e21[3].checked) {
-erradol3.style.color= "#ff0000"
+errado13.style.color= "#ff0000"
 }
 if (document.form2.e21[4].checked) {
-erradol4.style.color= "#ff0000"
+errado14.style.color= "#ff0000"
 }
 
-if (document.form2.e22[0]. checked) {
+if (document.form2.e22[0].checked) {
 errado20.style.color= "#ff0000"
 }
-if (document.form2.e22[1]. checked) {
+if (document.form2.e22[1].checked) {
 errado21.style.color= "#ff0000"
 }
 if (document.form2.e22[2].checked) {
 score= score + 1;
 certo2.style.color="#00ff00"
 }
-if (document.form2.e22[3]. checked) {
+if (document.form2.e22[3].checked) {
 errado23.style.color= "#ff0000"
 }
-if (document.form2.e22[4]. checked) {
+if (document.form2.e22[4].checked) {
 errado24.style.color= "#ff0000"
 }
 
-if (document.form2.e23[0]. checked) {
+if (document.form2.e23[0].checked) {
 errado30.style.color= "#ff0000"
 }
 if (document.form2.e23[1].checked) {
 score= score + 1;
 certo3.style.color="#00ff00"
 }
-if (document.form2.e23[2]. checked) {
+if (document.form2.e23[2].checked) {
 errado32.style.color= "#ff0000"
 }
-if (document.form2.e23[3]. checked) {
+if (document.form2.e23[3].checked) {
 errado33.style.color= "#ff0000"
 }
-if (document.form2.e23[4]. checked) {
+if (document.form2.e23[4].checked) {
 errado34.style.color= "#ff0000"
 }
 
-if (document.form2.e24[0]. checked) {
+if (document.form2.e24[0].checked) {
 errado40.style.color= "#ff0000"
 }
-if (document.form2.e24[1]. checked) {
+if (document.form2.e24[1].checked) {
 errado41.style.color= "#ff0000"
 }
-if (document.form2.e24[2]. checked) {
+if (document.form2.e24[2].checked) {
 errado42.style.color= "#ff0000"
 }
-if (document.form2.e24[3]. checked) {
+if (document.form2.e24[3].checked) {
 errado43.style.color= "#ff0000"
 }
 if (document.form2.e24[4].checked) {
@@ -73,46 +73,46 @@ if (document.form2.e25[0].checked) {
 score= score + 1;
 certo5.style.color="#00ff00"
 }
-if (document.form2.e25[1]. checked) {
+if (document.form2.e25[1].checked) {
 errado51.style.color= "#ff0000"
 }
-if (document.form2.e25[2]. checked) {
+if (document.form2.e25[2].checked) {
 errado52.style.color= "#ff0000"
 }
-if (document.form2.e25[3]. checked) {
+if (document.form2.e25[3].checked) {
 errado53.style.color= "#ff0000"
 }
-if (document.form2.e25[4]. checked) {
+if (document.form2.e25[4].checked) {
 errado54.style.color= "#ff0000"
 }
 
-if (document.form2.e26[0]. checked) {
+if (document.form2.e26[0].checked) {
 errado60.style.color= "#ff0000"
 }
 if (document.form2.e26[1].checked) {
 score= score + 1;
 certo6.style.color="#00ff00"
 }
-if (document.form2.e26[2]. checked) {
+if (document.form2.e26[2].checked) {
 errado62.style.color= "#ff0000"
 }
-if (document.form2.e26[3]. checked) {
+if (document.form2.e26[3].checked) {
 errado63.style.color= "#ff0000"
 }
-if (document.form2.e26[4]. checked) {
+if (document.form2.e26[4].checked) {
 errado64.style.color= "#ff0000"
 }
 
-if (document.form2.e27[0]. checked) {
+if (document.form2.e27[0].checked) {
 errado70.style.color= "#ff0000"
 }
-if (document.form2.e27[1]. checked) {
+if (document.form2.e27[1].checked) {
 errado71.style.color= "#ff0000"
 }
-if (document.form2.e27[2]. checked) {
+if (document.form2.e27[2].checked) {
 errado72.style.color= "#ff0000"
 }
-if (document.form2.e27[3]. checked) {
+if (document.form2.e27[3].checked) {
 errado73.style.color= "#ff0000"
 }
 if (document.form2.e27[4].checked) {
@@ -120,20 +120,20 @@ score= score + 1;
 certo7.style.color="#00ff00"
 }
 
-if (document.form2.e28[0]. checked) {
+if (document.form2.e28[0].checked) {
 errado80.style.color= "#ff0000"
 }
-if (document.form2.e28[1]. checked) {
+if (document.form2.e28[1].checked) {
 errado81.style.color= "#ff0000"
 }
 if (document.form2.e28[2].checked) {
 score= score + 1;
 certo8.style.color="#00ff00"
 }
-if (document.form2.e28[3]. checked) {
+if (document.form2.e28[3].checked) {
 errado83.style.color= "#ff0000"
 }
-if (document.form2.e28[4]. checked) {
+if (document.form2.e28[4].checked) {
 errado84.style.color= "#ff0000"
 }
 alert ("Você acertou "+score+" de 8 questões!");
