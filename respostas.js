1f80e37dc4847828beb06076536610514777e4e8
Rayssa <115266145+Ray4835@users.noreply.github.com>
2023-04-11 21:42:05 -0300

    Update respostas.js

--- respostas.js
+++ respostas.js
@@ -3,137 +3,138 @@ score=0;
 
 if (document.form2.e21[0].checked) {
 score= score + 1;
-(window.certo1.style.color="00ff00")
+certo1.style.color="#00ff00"
 }
-    if (document.form2.e21[1]. checked) {
-    (window.erradoll.style.color= "ff0000")
+if (document.form2.e21[1].checked) {
+erradoll.style.color= "#ff0000"
 }
-if (document.form2.e21[2]. checked) {
-(window.erradol2.style.color= "ff0000")
+if (document.form2.e21[2].checked) {
+erradol2.style.color= "#ff0000"
 }
-if (document.form2.e21[3]. checked) {
-(window.erradol3.style.color= "ff0000")
+if (document.form2.e21[3].checked) {
+erradol3.style.color= "#ff0000"
 }
-if (document.form2.e21[4]. checked) {
-(window.erradol4.style.color= "ff0000")
+if (document.form2.e21[4].checked) {
+erradol4.style.color= "#ff0000"
 }
 
 if (document.form2.e22[0]. checked) {
-(window.errado20.style.color= "ff0000")
+errado20.style.color= "#ff0000"
 }
 if (document.form2.e22[1]. checked) {
-(window.errado21.style.color= "ff0000")
+errado21.style.color= "#ff0000"
 }
 if (document.form2.e22[2].checked) {
 score= score + 1;
-(window.certo2.style.color="00ff00"){
+certo2.style.color="#00ff00"
 }
 if (document.form2.e22[3]. checked) {
-(window.errado23.style.color= "ff0000")
+errado23.style.color= "#ff0000"
 }
 if (document.form2.e22[4]. checked) {
-(window.errado24.style.color= "ff0000")
+errado24.style.color= "#ff0000"
 }
 
 if (document.form2.e23[0]. checked) {
-(window.errado30.style.color= "ff0000")
+errado30.style.color= "#ff0000"
 }
 if (document.form2.e23[1].checked) {
 score= score + 1;
-(window.certo3.style.color="00ff00")
+certo3.style.color="#00ff00"
 }
 if (document.form2.e23[2]. checked) {
-(window.errado32.style.color= "ff0000")
+errado32.style.color= "#ff0000"
 }
 if (document.form2.e23[3]. checked) {
-(window.errado33.style.color= "ff0000")
+errado33.style.color= "#ff0000"
 }
 if (document.form2.e23[4]. checked) {
-(window.errado34.style.color= "ff0000")
+errado34.style.color= "#ff0000"
 }
 
 if (document.form2.e24[0]. checked) {
-(window.errado40.style.color= "ff0000")
+errado40.style.color= "#ff0000"
 }
 if (document.form2.e24[1]. checked) {
-(window.errado41.style.color= "ff0000")
+errado41.style.color= "#ff0000"
 }
 if (document.form2.e24[2]. checked) {
-(window.errado42.style.color= "ff0000")
+errado42.style.color= "#ff0000"
 }
 if (document.form2.e24[3]. checked) {
-(window.errado43.style.color= "ff0000")
+errado43.style.color= "#ff0000"
 }
 if (document.form2.e24[4].checked) {
 score= score + 1;
-(window.certo4.style.color="00ff00")
+certo4.style.color="#00ff00"
 }
 
 if (document.form2.e25[0].checked) {
 score= score + 1;
-(window.certo5.style.color="00ff00")
+certo5.style.color="#00ff00"
 }
 if (document.form2.e25[1]. checked) {
-(window.errado51.style.color= "ff0000")
+errado51.style.color= "#ff0000"
 }
 if (document.form2.e25[2]. checked) {
-(window.errado52.style.color= "ff0000")
+errado52.style.color= "#ff0000"
 }
 if (document.form2.e25[3]. checked) {
-(window.errado53.style.color= "ff0000")
+errado53.style.color= "#ff0000"
 }
 if (document.form2.e25[4]. checked) {
-(window.errado54.style.color= "ff0000")
+errado54.style.color= "#ff0000"
 }
 
 if (document.form2.e26[0]. checked) {
-(window.errado60.style.color= "ff0000")
+errado60.style.color= "#ff0000"
 }
 if (document.form2.e26[1].checked) {
 score= score + 1;
-(window.certo6.style.color="00ff00")
+certo6.style.color="#00ff00"
 }
 if (document.form2.e26[2]. checked) {
-(window.errado62.style.color= "ff0000")
+errado62.style.color= "#ff0000"
 }
 if (document.form2.e26[3]. checked) {
-(window.errado63.style.color= "ff0000")
+errado63.style.color= "#ff0000"
 }
 if (document.form2.e26[4]. checked) {
-(window.errado64.style.color= "ff0000")
+errado64.style.color= "#ff0000"
 }
 
 if (document.form2.e27[0]. checked) {
-(window.errado70.style.color= "ff0000")
+errado70.style.color= "#ff0000"
 }
 if (document.form2.e27[1]. checked) {
-(window.errado71.style.color= "ff0000")
+errado71.style.color= "#ff0000"
 }
 if (document.form2.e27[2]. checked) {
-(window.errado72.style.color= "ff0000")
+errado72.style.color= "#ff0000"
 }
 if (document.form2.e27[3]. checked) {
-(window.errado73.style.color= "ff0000")
+errado73.style.color= "#ff0000"
 }
 if (document.form2.e27[4].checked) {
 score= score + 1;
-(window.certo7.style.color="00ff00")
+certo7.style.color="#00ff00"
 }
 
 if (document.form2.e28[0]. checked) {
-(window.errado80.style.color= "ff0000")
+errado80.style.color= "#ff0000"
 }
 if (document.form2.e28[1]. checked) {
-(window.errado81.style.color= "ff0000")
+errado81.style.color= "#ff0000"
 }
 if (document.form2.e28[2].checked) {
 score= score + 1;
-(window.certo8.style.color="00ff00")
+certo8.style.color="#00ff00"
 }
 if (document.form2.e28[3]. checked) {
-(window.errado83.style.color= "ff0000")
+errado83.style.color= "#ff0000"
 }
 if (document.form2.e28[4]. checked) {
-(window.errado84.style.color= "ff0000")
+errado84.style.color= "#ff0000"
 }
-alert ("Você acertou "+score+" de 12 questões!");
+alert ("Você acertou "+score+" de 8 questões!");
+}
